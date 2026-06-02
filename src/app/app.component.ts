40eeda8913644c48d293f0a843e28325f42d79d5
savaged-22 <juanpabloespinosa2014@gmail.com>
2026-06-02 17:55:50 -0500

    Este es mi ultimo esfuerzo ni lo compile profe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,6 @@ import { Component, signal } from '@angular/core';
 export class AppComponent {
   protected readonly title = signal('parcial-frontend');
 
-  // funcion q se ejecuta cuando le das click al boton rojo
   public clickBoton() {
     alert("todavia no hay mas torneos, perdon :(");
     console.log("el user hizo click en el boton de ver mas");
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TorneosComponent } from './components/torneos/torneos.component';
+import { EquiposComponent } from './components/equipos/equipos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TorneosComponent,
+    EquiposComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
-    provideClientHydration(withEventReplay())
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(withFetch())
   ],
   bootstrap: [AppComponent]
 })
